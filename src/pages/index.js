1d76fff97e050e353d2a2dfa4d2a7ddd8c69369e
Sofia <sofia.poulsen@colorado.edu>
2023-11-18 09:57:44 -0700

    removed extra stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,23 +10,12 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className='title'>Congratulations! You've set up your webapp!</h1>
+        <h1 className='title'>Sofia, Melanie and Evan's Website</h1>
 
         <p className='description'>
-          Get started by editing <code>src/pages/index.js</code> to be your
-          project home page.
+          Welcome to our cat cafe!
         </p>
-        <p className='description2'>
-          Next, each team member will create their own about me file in the
-          pages directory:{' '}
-        </p>
-        <code>src/page/teamMemberName.js</code>
-
         <div className='grid'>
-          <a href='/TomHolland ' className='card'>
-            <h3>Example About Me Page &rarr;</h3>
-            <p>Click here to navigate to an example page</p>
-          </a>
 
           <a href='/SofiaP ' className='SofiaCard'>
             <h3>Sofia's Page &rarr;</h3>
@@ -40,38 +29,11 @@ export default function Home() {
             <h3>Melanie's Page &rarr;</h3>
             <p>I really like opossums :)</p>
           </a>
+          <a href='/TomHolland ' className='card'>
+            <h3>Example About Me Page &rarr;</h3>
+            <p>Click here to navigate to an example page</p>
+          </a>
 
-
-          <a
-            href='https://github.com/vedant29/Project-CSCI-1000'
-            className='card'>
-            <h3>Project Resources&rarr;</h3>
-            <p>
-              Return to the Github for resources on creating your own website!
-            </p>
-          </a>
-          <a
-            href='https://nextjs.org/learn/foundations/about-nextjs'
-            className='card'>
-            <h3>Next.js&rarr;</h3>
-            <p>Click here to learn more about Next.js</p>
-          </a>
-          <a href='https://blog.hubspot.com/website/html' className='card'>
-            <h3>HTML and CSS &rarr;</h3>
-            <p>Click here to learn more about HTML and CSS</p>
-          </a>{' '}
-          <a href='https://tailwindcss.com/' className='card'>
-            <h3>Tailwind css&rarr;</h3>
-            <p>Click here to learn more about tailwind css</p>
-          </a>
-          <a href='https://styled-components.com/' className='card'>
-            <h3>Styled Components&rarr;</h3>
-            <p>Click here to learn more about Styled Components</p>
-          </a>
-          <a href='https://www.netlify.com/' className='card'>
-            <h3>Netlify&rarr;</h3>
-            <p>Click here to learn more about Netlify to host your website</p>
-          </a>
         </div>
       </main>
 
